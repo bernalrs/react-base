a939739bf4545f5c6f34a36e51038c6d570aa50c
bernalrs <bernal.rs@gmail.com>
2017-02-26 21:54:09 +0000

    first cut.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/template/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                options:{
+                    presets:['es2015','react']
+                }
+            }
+        ]
+    }
+};
